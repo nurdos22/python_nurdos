3daa5e9a3679d4b863a9ba79ce179b6a6dd07e42
nurdos22 <nurdoskubatbekov@gmail.com>
2025-01-25 22:00:51 +0600

    Homework 6.3

--- Homework/main.py
+++ Homework/main.py
@@ -3,18 +3,18 @@ import configparser
 from logic import game_settings, play_game
 
 def main():
-    # Считываем настройки из settings.ini
+
     config = configparser.ConfigParser()
     config.read("settings.ini")
 
-    # Функция для получения значений настроек
+
     def get_setting(setting_name):
         return int(config["Game_GUESS"][setting_name])
 
-    # Получаем параметры игры
+
     range_start, range_end, start_capital = game_settings(get_setting)
 
-    # Запускаем игру
+
     play_game(range_start, range_end, start_capital)
 
 if __name__ == "__main__":
